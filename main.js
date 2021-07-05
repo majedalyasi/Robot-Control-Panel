0163d0251b22f50289ed9ee34cdf96e4532aef5e
majedalyasi <78644116+majedalyasi@users.noreply.github.com>
2021-07-05 11:58:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+  var sliderA = document.querySelector("#myRangeA");
+  var valueA = document.querySelector("#valueA");
+  valueA.innerHTML = sliderA.value;
+  sliderA.oninput = function() {
+      valueA.innerHTML = this.value;
+  }
+
+  var sliderB = document.querySelector("#myRangeB");
+  var valueB = document.querySelector("#valueB");
+  valueB.innerHTML = sliderB.value;
+  sliderB.oninput = function() {
+      valueB.innerHTML = this.value;
+  }
+
+  var sliderC = document.querySelector("#myRangeC");
+  var valueC = document.querySelector("#valueC");
+  valueC.innerHTML = sliderC.value;
+  sliderC.oninput = function() {
+      valueC.innerHTML = this.value;
+  }
+
+  var sliderC = document.querySelector("#myRangeC");
+  var valueC = document.querySelector("#valueC");
+  valueC.innerHTML = sliderC.value;
+  sliderC.oninput = function() {
+      valueC.innerHTML = this.value;
+  }
+
+  var sliderD = document.querySelector("#myRangeD");
+  var valueD = document.querySelector("#valueD");
+  valueD.innerHTML = sliderD.value;
+  sliderD.oninput = function() {
+      valueD.innerHTML = this.value;
+  }
+
+  var sliderE = document.querySelector("#myRangeE");
+  var valueE = document.querySelector("#valueE");
+  valueE.innerHTML = sliderE.value;
+  sliderE.oninput = function() {
+      valueE.innerHTML = this.value;
+  }
+
+  var sliderF = document.querySelector("#myRangeF");
+  var valueF = document.querySelector("#valueF");
+  valueF.innerHTML = sliderF.value;
+  sliderF.oninput = function() {
+      valueF.innerHTML = this.value;
+  }
